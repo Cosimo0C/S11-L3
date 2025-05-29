8996f079eaa2a19c12db15faeac19204f56757d3
Cosimo0C <cosimocrupi01@gmail.com>
2025-05-29 16:21:45 +0200

    S11-L3

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,17 +1,32 @@
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
-const Job = ({ data }) => (
-  <Row className="mx-0 mt-3 p-3" style={{ border: "1px solid #00000033", borderRadius: 4 }}>
-    <Col xs={3}>
-      <Link to={`/${data.company_name}`}>{data.company_name}</Link>
-    </Col>
-    <Col xs={9}>
-      <a href={data.url} target="_blank" rel="noreferrer">
-        {data.title}
-      </a>
-    </Col>
-  </Row>
-);
+const Job = ({ data }) => {
+  const dispach = useDispatch();
+  return (
+    <Row className="mx-0 mt-3 p-3" style={{ border: "1px solid #00000033", borderRadius: 4 }}>
+      <Col xs={3}>
+        <Link to={`/${data.company_name}`}>{data.company_name}</Link>
+      </Col>
+      <Col xs={9}>
+        <a href={data.url} target="_blank" rel="noreferrer">
+          {data.title}
+        </a>
+      </Col>
+      <Col xs={2}>
+        <Button
+          variant="danger"
+          onClick={() => {
+            dispach({ type: "PREFERITO", payload: data.company_name });
+          }}
+        >
+          {" "}
+          Preferito
+        </Button>
+      </Col>
+    </Row>
+  );
+};
 
 export default Job;
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,15 +1,30 @@
 //primo reduce deve essere funziona pura e prende lo stato attuale
+
+import { act } from "react";
+
 //si crea uno stato iniziale  per il primo avvio
 const inizialState = {
-  jobs: {
-    work: [],
+  preferiti: {
+    companies: [],
   },
 };
 const mainReducer = (state = inizialState, action) => {
   // qua in ogni caso va ritornato un nuovo oggetto di stato
   switch (action.type) {
-    case " ":
+    case "PREFERITO":
+      return {
+        ...state,
+        preferiti: {
+          companies: [...state.preferiti.companies, action.payload],
+        },
+      };
+    case "REMOVE-LIST":
+      return {
+        ...state,
+        companies: state.preferiti.companies.filter((comp) => comp.id !== action.payload),
+      };
     default:
+      console.log("default");
       return state;
   }
 };
